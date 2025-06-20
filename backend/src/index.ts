90fadfd4df23a5cf24dd2070fbb57563b6340fd3
24LP7027 24LP7027 <24LP7027@admins-iMac.local>
2025-06-20 15:14:00 +0800

    connection test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { connectDb } from "./mongoConnection";
+import { UserRouter } from "./routers/user.router";
 
 const app = express();
 app.use(cors());
@@ -12,6 +13,7 @@ connectDb();
 app.use("/service", (req, res) => {
   res.send("hello world");
 });
+app.use("/user", UserRouter);
 
 app.listen(port, () => {
   console.log(`successfuly listenin port ${port}`);
