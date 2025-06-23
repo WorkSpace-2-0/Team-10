76f0e84ff103240144b487c632b250574562aba6
24LP7027 24LP7027 <24LP7027@admins-iMac.local>
2025-06-23 17:42:32 +0800

    mood entry

--- frontend/src/app/(userComponents)/_feature/IndividualComponent.tsx
+++ frontend/src/app/(userComponents)/_feature/IndividualComponent.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import MoodEntry from "../_components/MoodEntry";
+
 export default function IndividualComponent() {
   return (
-    <div className="bg-blue-500 w-screen h-screen mt-[100px]">
-      user components
+    <div className="bg-blue-500 w-screen h-screen mt-[100px] flex justify-center">
+      <MoodEntry />
     </div>
   );
 }
